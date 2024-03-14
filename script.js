5fb35744159b404b6824125e726ed9624f07d6a3
uShire <afgregana@up.edu.ph>
2024-03-14 14:56:16 +0800

    header 100%

---
+++ script.js
@@ -0,0 +1,12 @@
+const Borgir = document.querySelector(".borgir_menu")
+const Hnav = document.querySelector(".header_nav")
+Borgir.addEventListener("click", () =>{
+    Borgir.classList.toggle("open"); 
+    Hnav.classList.toggle("close");
+})
+
+
+
+
+ 
+
